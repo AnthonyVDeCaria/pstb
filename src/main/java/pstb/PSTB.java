b3e40dfbe441aea00a107162f50b81b8bf7c4d5d
padres-dev-4187 <padres-dev-4187@MSRG-4187-2>
2017-09-07 18:02:19 -0400

    Began work on accessing the Benchmark variables. Started by handling the topology file.

--- src/main/java/pstb/PSTB.java
+++ src/main/java/pstb/PSTB.java
@@ -4,6 +4,10 @@
  */
 package pstb;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -14,57 +18,102 @@ import pstb.util.UI;
 public class PSTB {
 	private static final Logger logger = LogManager.getRootLogger();
 	
+	private static Properties loadProperties(String propertyFile, Properties defaultProperties) throws IOException {
+		Properties properties = new Properties(defaultProperties);
+		FileInputStream propFileStream = new FileInputStream(propertyFile);
+		properties.load(propFileStream);
+		propFileStream.close();
+		return properties;
+	}
+	
 	public static void main(String[] args)
 	{
-		String topologyFileName = "src/test/java/topologyTest.txt";
+		logger.info("Starting programm - getting properties.");
 		
-		TopologyFileParser parserTopo = new TopologyFileParser();
+		Properties defaultProp = null;
 		
-		logger.info("Starting Parse.");
-		boolean parseCheck = parserTopo.parse(topologyFileName);
-		if(!parseCheck)
+		try
 		{
-			logger.error("Parse Failed!");
+			defaultProp = loadProperties("etc/defaultBenchmark.properties", null);
 		}
-		else
+		catch (IOException e)
 		{
-			logger.info("Parse Complete.");
-			LogicalTopology network = parserTopo.getLogicalTopo();
+			logger.error("Couldn't load default properties file", e);
+		}
+		
+		if(defaultProp != null)
+		{
+			String topologyFileName = null;
 			
-			logger.info("Starting Topology Testing.");
-			boolean mCCheck = network.confirmBrokerMutualConnectivity();
-			if(!mCCheck)
+			String customBenchProp = "Would you like to use a custom benchmark properties file Y/n?";
+			boolean customBenchPropAns = UI.getYNAnswerFromUser(customBenchProp);
+			if (!customBenchPropAns)
 			{
-				logger.info("Topology File has one way connections.");
-				
-				String fixBroker = "Would you like to fix Y/n?\n"
-						+ "Answering 'n' will terminate this program.";
-				boolean fixBrokerAns = UI.getYNAnswerFromUser(fixBroker);
-				if (!fixBrokerAns)
-				{
-					System.exit(0);
-				}
-				else
-				{
-					try
-					{
-						network.forceMutualConnectivity();
-					}
-					catch(IllegalArgumentException e)
-					{
-						logger.warn("Problem forcing mutual connectivity", e);
-					}
-				}
-			}
-			
-			boolean topoCheck = network.confirmTopoConnectivity();
-			if(!topoCheck)
-			{
-				logger.error("Topology Check Failed!");
+				topologyFileName = defaultProp.getProperty("pstb.topologyFileLocation");
 			}
 			else
 			{
-				logger.info("Topology Check Complete.");
+				try
+				{
+					Properties userProp = loadProperties("src/test/java/userBenchmark.properties", defaultProp);
+					topologyFileName = userProp.getProperty("pstb.topologyFileLocation");
+				}
+				catch (IOException e)
+				{
+					logger.error("Couldn't load user properties file", e);
+				}
+			}
+			
+			if(topologyFileName != null)
+			{
+				logger.info("Starting Topology File Parse.");
+				TopologyFileParser parserTopo = new TopologyFileParser();
+				boolean parseCheck = parserTopo.parse(topologyFileName);
+				if(!parseCheck)
+				{
+					logger.error("Parse Failed!");
+				}
+				else
+				{
+					logger.info("Parse Complete.");
+					LogicalTopology network = parserTopo.getLogicalTopo();
+					
+					logger.info("Starting Topology Testing.");
+					boolean mCCheck = network.confirmBrokerMutualConnectivity();
+					if(!mCCheck)
+					{
+						logger.info("Topology File has one way connections.");
+						
+						String fixBroker = "Would you like to fix Y/n?\n"
+								+ "Answering 'n' will terminate this program.";
+						boolean fixBrokerAns = UI.getYNAnswerFromUser(fixBroker);
+						if (!fixBrokerAns)
+						{
+							System.exit(0);
+						}
+						else
+						{
+							try
+							{
+								network.forceMutualConnectivity();
+							}
+							catch(IllegalArgumentException e)
+							{
+								logger.warn("Problem forcing mutual connectivity", e);
+							}
+						}
+					}
+					
+					boolean topoCheck = network.confirmTopoConnectivity();
+					if(!topoCheck)
+					{
+						logger.error("Topology Check Failed!");
+					}
+					else
+					{
+						logger.info("Topology Check Complete.");
+					}
+				}
 			}
 		}
 	}
