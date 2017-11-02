5c65f9d9a5edd829dd24c012bba6a1920709969f
padres-dev-4187 <padres-dev-4187@MSRG-4187-2>
2017-11-02 16:45:18 -0400

    Fixed a spelling mistake in DistributedFlagValue.

--- src/main/java/pstb/util/DistributedFlagValue.java
+++ src/main/java/pstb/util/DistributedFlagValue.java
@@ -8,7 +8,7 @@ package pstb.util;
  * 
  * The Values a Distributed Flag can take.
  * The Distributed Flag is a ClientDiary Name flag that signifies if this Diary came from a distributed experiment.
- * D means distrubted, L means local.
+ * D means distributed, L means local.
  */
 public enum DistributedFlagValue {
 	D, L
