afbf5022b5a61727c8c9dac34e263cd82f622c0d
padres-dev-4187 <padres-dev-4187@MSRG-4187-2>
2017-11-02 16:42:09 -0400

    Added documentation to DistributedFlagValue.

--- src/main/java/pstb/util/DistributedFlagValue.java
+++ src/main/java/pstb/util/DistributedFlagValue.java
@@ -5,7 +5,10 @@ package pstb.util;
 
 /**
  * @author padres-dev-4187
- *
+ * 
+ * The Values a Distributed Flag can take.
+ * The Distributed Flag is a ClientDiary Name flag that signifies if this Diary came from a distributed experiment.
+ * D means distrubted, L means local.
  */
 public enum DistributedFlagValue {
 	D, L
